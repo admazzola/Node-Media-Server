ad6d4a8125578906a6547252a29d019e247f1987
Andy <admazzola@gmail.com>
2020-06-23 00:14:55 -0400

    half scale

--- node_trans_session.js
+++ node_trans_session.js
@@ -80,7 +80,7 @@ class NodeTransSession extends EventEmitter {
     Array.prototype.push.apply(argv, this.conf.vcParam);
     Array.prototype.push.apply(argv, ['-c:a', ac]);
     Array.prototype.push.apply(argv, this.conf.acParam);
-    Array.prototype.push.apply(argv, ['-crf','28']);
+    Array.prototype.push.apply(argv, ["-vf" ,"scale=iw/2:ih/2"]);
     Array.prototype.push.apply(argv, ['-f', 'tee', '-map', '0:a?', '-map', '0:v?', mapStr]);
     argv = argv.filter((n) => { return n }); //去空
     this.ffmpeg_exec = spawn(this.conf.ffmpeg, argv);
